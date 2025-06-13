0b3bc78401e47d574b96408b3d511ee9ff3f6062
sungu <cliffland.jasungu@gmail.com>
2025-06-13 14:23:40 +0300

    correct

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,10 +1,7 @@
-const [ , , firstArg, secondArg] = process.argv;
+const [ , , firstArg] = process.argv;
 
 if (!firstArg) {
     console.log ("No argument");
-} else if (!secondArg) {
-    console.log (firstArg);
 } else {
     console.log (firstArg);
-    console.log (secondArg);
 }
