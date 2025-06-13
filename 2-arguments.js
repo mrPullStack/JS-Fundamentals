49d90c25201afd9f5d82f87c70703dd04a038925
sungu <cliffland.jasungu@gmail.com>
2025-06-13 14:02:58 +0300

    correct

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,7 +3,7 @@ const args = process.argv.slice(2);
 if (args.length === 0) {
     console.log("No argument");
 } else if (args.length === 1) {
-    console.log("1 Argument found");
+    console.log("Argument found");
 } else {
-    console.log("3 Argument found");
+    console.log("Arguments found");
 }
